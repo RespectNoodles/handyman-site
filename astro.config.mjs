cc54ca7b8b04322d2e8ba7d540863df094939957
RespectNoodles <cruzegan@gmail.com>
2025-06-18 17:12:54 +0800

    Added sentry/astro & spotlightjs/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import { fileURLToPath } from "url";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), sentry(), spotlightjs()],
   platformProxy: {
     enabled: true,
   },
