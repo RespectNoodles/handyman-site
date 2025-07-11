6e38951c75694ac9c4ed562b29d57b2c8ac5e110
RespectNoodles <cruzegan@gmail.com>
2025-07-11 09:38:14 +0800

    Making final changes before deploying site

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,11 +3,12 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/pages/blog", pattern: "**/*.{astro,md,mdx}" }),
+  loader: glob({ base: "/src/pages/blog", 
+  pattern: "**/*.{astro,md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: z.object({
-    title: z.string(),
-    description: z.string(),
+  title: z.string(),
+  description: z.string(),
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
